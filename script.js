6a516689d7059b32e203a5c5574812893ffc8107
AmalKrishna <121586027+AmalKrishnaS@users.noreply.github.com>
2025-03-22 13:22:09 +0000

    feat: Add form validation and error display
    - Added a `<div class="error-box">` for displaying validation errors
    - Styled the error box with red background and white text
    - Initially hidden using the `hidden` attribute
    - Implemented validation for title, author, and year to prevent empty
      inputs
    - Limited description input to 60 characters
    - Trimmed input values to remove accidental spaces
    - Displayed multiple validation errors dynamically in the error box

--- script.js
+++ script.js
@@ -23,24 +23,51 @@ function runMyLibrary() {
     const submitButton = document.querySelector('[type="submit"]');
     const cancelButton = document.querySelector('[type="button"]');
     const form = document.querySelector("form");
+    const errorBox = document.querySelector(".error-box");
 
     addBookButton.addEventListener("click", function () {
         formDialog.showModal();
     })
 
+    function checkValidation(title, author, year, description) {
+        let errors = [];
+        
+        if (title==="" || author==="" || year==="") {
+            errors.push("Title, Author, Year shouldn't be empty");
+        }
+
+        if (description.length > 60) {
+            errors.push("Description should be less than 60 characters");
+        }
+
+        return errors;
+    }
+
     submitButton.addEventListener("click", function (event) {
         event.preventDefault();
         
         const formData = new FormData(form);
-        
-        addBookToLibrary(formData.get("title"),
-        formData.get("author"),
-        formData.get("year"),
-        formData.get("status"),
-        formData.get("description"));
 
-        formDialog.close();
-        form.reset();
+        const title = formData.get("title").trim();
+        const author = formData.get("author").trim();
+        const year = formData.get("year").trim();
+        const status = formData.get("status").trim();
+        const description = formData.get("description").trim();
+
+        let errors = checkValidation(title, author, year, description);
+
+        if (errors.length > 0) {
+            errorBox.innerHTML = errors.join("<br>");
+            errorBox.hidden = false;
+        }
+        else {
+            addBookToLibrary(title, author, year, status, description);
+
+            formDialog.close();
+            errorBox.hidden = true;
+            form.reset();
+        }
+        
     });
 
     cancelButton.addEventListener("click", () => {
